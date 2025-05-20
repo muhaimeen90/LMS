15056e70e6c0cf9c0e37500ce3e3a97becbf5233
Piccolo7210 <mustakim.mohsin7210@gmail.com>
2025-05-20 23:10:09 +0600

    chatbot backend part added

---
+++ backend/services/chatbotService.js
@@ -0,0 +1,24 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
+
+export const chatWithAI = async (req, res) => {
+    const userMessage = req.body.message;
+    try {
+        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+        const result = await model.generateContent(userMessage);
+        const response = await result.response;
+        const text = response.text();
+
+        res.status(200).json({ response: text });
+    } catch (error) {
+        console.error("Error generating response:", error);
+        res.status(500).json({ error: "Error generating response" });
+    }
+};
